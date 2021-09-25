bcec4816767dfbb61d0fa28d6c0254362157c8ba
Alexsandro G Bezerra <alexsandro.bezerra@stone.com.br>
2021-09-25 12:11:53 -0300

    feat: add prisma

---
+++ prisma/migrations/20210925150655_create_professor/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Professor" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Professor_email_key" ON "Professor"("email");
